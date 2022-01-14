bc449550e0324297877654240611dce3a58ebcfc
hamzaayoub98 <hamza.ayoub@etu.univ-cotedazur.fr>
2022-01-14 14:59:18 +0100

    update server

--- back-end/index.js
+++ back-end/index.js
@@ -4,3 +4,7 @@ const app = express()
 app.listen(8080, () => {
     console.log('Serveur à l\'écoute')
   })
+
+app.get('/', function(req, res) {
+    res.status(200).send('Bienvenue à bord !');
+});
