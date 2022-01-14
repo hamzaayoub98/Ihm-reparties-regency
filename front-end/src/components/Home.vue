e044588f288b2e5e5d89766e312a95d9da1c3e5d
Benjamin Coux <benjamin.coux@etu.unice.fr>
2022-01-14 15:09:46 +0100

    debut appel API

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="home">
+        <h1>Hi m8</h1>
+        <img alt="home screen" src="../assets/kc.png">
+        <div id="api">
+            <p>
+                {{info}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import  Axios from 'axios';
+    export default {
+        name: "Home",
+        data(){
+            return {
+                info : null,
+            }
+        },
+        mounted() {
+            Axios.get("https://api.themoviedb.org/3/person/popular?api_key=f8c59b73c44d9240c1ded0a07da0d5f5")
+            .then(response =>(this.info = response));
+        }
+    }
+</script>
+
+<style scoped>
+    #home{
+    color: #ff0028;
+    }
+    #api{
+        color: chartreuse;
+    }
+</style>
