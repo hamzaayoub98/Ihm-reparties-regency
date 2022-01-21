43de2ed2bd360d67613eae153dbc9f6e80907880
Benjamin Coux <benjamin.coux@etu.unice.fr>
2022-01-21 14:44:51 +0100

    ajout liste evenement et supression de celle-ci depuis le back

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,8 @@
 const express = require('express')
-const app = express()
-var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-var cors = require('cors')
-app.use(cors())
-app.use(bodyParser.json())
-let actionStack = [1,2,3]
-
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const http = require("http");
+const WebSocket = require("ws");
 const baseActions = [{
     title:"Appuyez sur le bouton 1",
     id:1,
@@ -18,21 +14,60 @@ const baseActions = [{
         title :"Remettre de l`antimatière",
         id: 3,
     }]
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(bodyParser.json())
 
-app.listen(3000, () => {
-    console.log('Serveur à l\'écoute')
-  })
+const PORT_REST = 3000
+const PORT_WS = 4000
+
+const server = http.createServer(app);
+const websocketServer = new WebSocket.Server({ server });
+
+let actionStack = []
+var sockets = [];
+
+websocketServer.on('connection', function (socket) {
+    socket.on('message', msg => {
+        console.log(`Message: ${msg}`);
+        socket.send("Let's go !")
+    });
+})
+app.listen(PORT_REST, () => {
+    console.log('Serveur REST à l\'écoute ! port', PORT_REST)
+})
+server.listen(PORT_WS, () => {
+    console.log("Serveur WS à l\'écoute ! port", PORT_WS);
+});
+
+let gameStarted = false;
 
 app.get('/', function(req, res) {
-    res.status(200).send('Bienvenue à bord !');
+    res.status(200).json({"helloworld": "Hello World !"});
+});
+
+app.post('/start', (req, res) => {
+    res.status(200).json('ok');
+    sockets.forEach((s) => s.send("Décollage !"));
+});
+
+app.get('/game/status', (req, res) => {
+    res.status(200).json({"started": gameStarted});
+});
+
+app.post('/start/game', function(request, response){
+    this.gameStarted = request.body.started;
+    console.log("🚀 ~ file: index.js ~ line 51 ~ app.post ~ this.gameStarted", this.gameStarted)
+    response.status(200).send("data game statis received")
 });
 
 app.post('/button', function(request, response){
     processAction(request.body.action)
-    console.log(actionStack)
     console.log(baseActions)
     response.status(200).send("data received")
 });
+
 
 function processAction(action){
     if(actionStack.includes(action)){
